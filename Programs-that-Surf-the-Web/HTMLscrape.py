e07e626cbfcce6a66be465fa9b2b8493477a8519
Devin Maiello <maielld1@tcnj.edu>
2016-04-17 01:01:34 -0400

    Update HTMLscrape.py

--- Programs-that-Surf-the-Web/HTMLscrape.py
+++ Programs-that-Surf-the-Web/HTMLscrape.py
@@ -1,10 +1,15 @@
+#link1: http://python-data.dr-chuck.net/comments_42.html
+#link2: http://python-data.dr-chuck.net/comments_264800.html
+#answer1: sum=2553
+#answer2: sum=2507
+
 import urllib
 from bs4 import BeautifulSoup
 
 url = raw_input('Enter - ')
 html = urllib.urlopen(url).read()
 
-soup = BeautifulSoup(html)
+soup = BeautifulSoup(html, "html.parser")
 
 # Retrieve all of the anchor tags
 tags = soup('span')
