a798d031594a2c4193fd3a305afce1fc8b3588b1
Devin Maiello <maielld1@tcnj.edu>
2016-04-17 01:03:18 -0400

    Update FollowLinks.py

--- Programs-that-Surf-the-Web/FollowLinks.py
+++ Programs-that-Surf-the-Web/FollowLinks.py
@@ -1,3 +1,11 @@
+#Sample problem: Start at http://python-data.dr-chuck.net/known_by_Fikret.html 
+#Find the link at position 3 (the first name is 1). Follow that link. Repeat this process 4 times. The answer is the last name that you retrieve.
+#Sequence of names: Fikret Montgomery Mhairade Butchi Anayah 
+#Last name in sequence: Anayah
+#Actual problem: Start at: http://python-data.dr-chuck.net/known_by_Kenan.html 
+#Find the link at position 18 (the first name is 1). Follow that link. Repeat this process 7 times. The answer is the last name that you retrieve.
+#Hint: The first character of the name of the last page that you will load is: C
+
 import urllib
 from bs4 import BeautifulSoup
 
